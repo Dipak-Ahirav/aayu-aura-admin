32ed65d63b9e119a7a9e7a34460993b6cadf532c
dipakahirav <dipaksahirav@gmail.com>
2026-07-19 20:56:52 +0530

    Env changes

--- apps/customer-web/src/environments/environment.production.ts
+++ apps/customer-web/src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://aayu-aura.onrender.com/api/v1',
+  apiBaseUrl: 'https://aayu-aura-admin.onrender.com/api/v1',
   customerWebUrl: 'https://aayu-aura-admin-admin-web.vercel.app',
   features: {
     guestCheckout: true,
